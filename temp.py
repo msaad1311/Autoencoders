7291f42d073acd05f8618823060b2f7bfe174b0d
Muhammad Saad <50722220+msaad1311@users.noreply.github.com>
2020-10-29 13:03:15 -0400

    a temporary python file being made
    The file is being made to test the video aspect

---
+++ temp.py
@@ -0,0 +1,11 @@
+from cv2 import cv2
+import os
+import numpy as np
+
+path = r'C:\Users\Saad.LAKES\Desktop\Autoencoders\ReconVideo_Frames'
+path = path.replace('\\','/')
+
+os.chdir(path)
+
+imgs =[]
+ls 
