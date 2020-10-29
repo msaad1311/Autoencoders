627582d0af3c5e3f13ecd105ba4b461dd3148717
Muhammad Saad <50722220+msaad1311@users.noreply.github.com>
2020-10-29 12:40:23 -0400

    Updated the file
    The notebook is being updated and then a python script is being created to run effectively. The weights and the rest of the things are being saved . Since the weights file is big, so it is being added to gitignore

--- Autoencoder.py
+++ Autoencoder.py
@@ -1,10 +1,29 @@
-from tensorflow.keras.layers import Dense, Flatten, Reshape, Input, InputLayer,Conv2D,MaxPool2D,UpSampling2D,Conv2DTranspose
-from tensorflow.keras.models import Sequential, Model
-from skimage.metrics import structural_similarity as ssim
 import numpy as np
+from cv2 import cv2
+import os
+from numba import cuda
 import matplotlib.pyplot as plt
+import random
+from skimage.metrics import structural_similarity as ssim
 
-def build_autoencoder(img_shape, code_size,name):
+from sklearn.model_selection import train_test_split
+
+import tensorflow as tf
+from tensorflow.keras.layers import Dense, Flatten, Reshape, Input, InputLayer,Conv2D,MaxPool2D,UpSampling2D,Conv2DTranspose,Cropping2D
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.callbacks import ModelCheckpoint
+os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        tf.config.experimental.set_virtual_device_configuration(gpus[0],
+                                                                [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=(1024*4))])
+        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+        print(e)
+
+def build_autoencoder(img_shape, code_size,name,shape_req):
     if name == 'Simple':
         # The encoder
         encoder = Sequential()
@@ -20,17 +39,41 @@ def build_autoencoder(img_shape, code_size,name):
 
         return encoder, decoder
     
+    elif name=='CNN2':
+        encoder = Sequential()
+        encoder.add(Conv2D(128,3,strides=2,input_shape=(shape_req,shape_req,3)))
+        encoder.add(Conv2D(128,3,strides=2))
+        encoder.add(Conv2D(128,3,strides=2))
+        encoder.add(Conv2D(128,3,strides=1))
+        encoder.add(Flatten())
+        encoder.add(Dense(code_size))
+
+        shape_pooling = encoder.layers[-3].output_shape
+        shape_flatten = encoder.layers[-2].output_shape
+
+        decoder = Sequential()
+        decoder.add(Dense(shape_flatten[1],input_shape=(code_size,)))
+        decoder.add(Reshape((shape_pooling[1],shape_pooling[2],shape_pooling[3])))
+        decoder.add(Conv2DTranspose(128,3,strides=1))
+        decoder.add(Conv2DTranspose(128,3,strides=2))
+        decoder.add(Conv2DTranspose(128,3,strides=2))
+        decoder.add(Conv2DTranspose(128,3,strides=2))
+        decoder.add(Conv2DTranspose(3,3,strides=1))
+        decoder.add(Cropping2D(cropping=((1, 0), (1, 0)), data_format=None))
+        
+        return encoder,decoder
+    
     elif name == 'CNN':
 
         pad='valid'
         img_shape=(128,128,3)
-        code_size=512
+        # code_size=512
         encoder = Sequential()
-        encoder.add(Conv2D(filters=128,kernel_size=3,input_shape=(img_shape)))
+        encoder.add(Conv2D(filters=32,kernel_size=3,input_shape=(img_shape)))
         encoder.add(MaxPool2D(pool_size=(2,2)))
-        encoder.add(Conv2D(filters=64,kernel_size=3))
+        encoder.add(Conv2D(filters=16,kernel_size=3))
         encoder.add(MaxPool2D(pool_size=(2,2)))
-        encoder.add(Conv2D(filters=32,kernel_size=3))
+        encoder.add(Conv2D(filters=8,kernel_size=3))
         encoder.add(MaxPool2D(pool_size=(2,2)))
         encoder.add(Flatten())
         encoder.add(Dense(code_size))
@@ -42,11 +85,11 @@ def build_autoencoder(img_shape, code_size,name):
         decoder.add(Dense(shape_flatten[1],input_shape=(code_size,)))
         decoder.add(Reshape((shape_pooling[1],shape_pooling[2],shape_pooling[3])))
         decoder.add(UpSampling2D(size=(2,2)))
+        decoder.add(Conv2DTranspose(filters=8,kernel_size=3,padding=pad))
+        decoder.add(UpSampling2D(size=(2,2)))
+        decoder.add(Conv2DTranspose(filters=16,kernel_size=3,padding=pad))
+        decoder.add(UpSampling2D(size=(2,2)))
         decoder.add(Conv2DTranspose(filters=32,kernel_size=3,padding=pad))
-        decoder.add(UpSampling2D(size=(2,2)))
-        decoder.add(Conv2DTranspose(filters=64,kernel_size=3,padding=pad))
-        decoder.add(UpSampling2D(size=(2,2)))
-        decoder.add(Conv2DTranspose(filters=128,kernel_size=3,padding=pad))
         # decoder.add(UpSampling2D(size=(2,2)))
         decoder.add(Conv2DTranspose(filters=3,kernel_size=3,padding=pad))
 
@@ -110,3 +153,78 @@ def overall_ssim(actual,predicted):
         results.append(s)
     results = np.array(results)
     return np.mean(results)
+
+def read_video(path,video_name):
+    
+    path = path.replace('\\','/')
+    cap = cv2.VideoCapture(video_name)
+    print('Loading Video...')
+    index = 0
+    successs,img = cap.read()
+
+    path_old = os.getcwd()
+    os.chdir(path)
+    names=[]
+
+    while successs:
+        cv2.imwrite('frame_{}.jpg'.format(index),img)
+        names.append('frame_{}.jpg'.format(index))
+        successs,img = cap.read()
+        index+=1
+        
+    os.chdir(path_old)
+    
+    print('Completed')
+    
+    return names
+
+def read_imgs(path,names,shape_req):
+    path = path.replace('\\','/')
+    path_old=os.getcwd()
+    os.chdir(path)
+    img =[]
+    for name in names:
+        temp = cv2.imread(name)
+        img.append(cv2.resize(temp,(shape_req,shape_req)))
+    os.chdir(path_old)
+    return img
+
+def splitter(x,testsize):
+    x_train,x_test = train_test_split(x,test_size=testsize,random_state=42)
+    return x_train,x_test
+
+def build_model(images,embedding_size,types,shape_req):
+    IMG_SHAPE = images.shape[1:]
+    embed_size = 1000
+    encoder, decoder = build_autoencoder(IMG_SHAPE, embed_size,types,shape_req)
+
+    inp = Input(IMG_SHAPE)
+    code = encoder(inp)
+    reconstruction = decoder(code)
+    
+    
+    autoencoder = Model(inp,reconstruction)
+    autoencoder.compile(optimizer='adam', loss='mse',metrics=['mae'])
+
+    print('[INFO] The model is created')
+    
+    return autoencoder,encoder,decoder
+    
+    
+def model_fit(model,x_train,x_test,epoch,batch_size=None):
+    filepath = 'weights.hdf5'
+    checkpoint = ModelCheckpoint(filepath,monitor='val_loss',verbose=0,save_best_only=True,mode=min)
+    
+    history = model.fit(x=x_train, y=x_train, epochs=epoch,validation_split=0.2,batch_size=batch_size,callbacks=[checkpoint])
+    
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+    
+    return
+
+    
---
+++ main.py
@@ -0,0 +1,76 @@
+## Libraries
+import numpy as np
+from cv2 import cv2
+import os
+import random
+
+from Autoencoder import *
+
+import tensorflow as tf
+
+os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        tf.config.experimental.set_virtual_device_configuration(gpus[0],
+                                                                [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=(1024*4))])
+        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+        print(e)
+        
+## PARAMETERS
+PATH =  r'C:\Users\Saad.LAKES\Desktop\Autoencoders\Video_Frames'
+PATH_RESULTS = r'C:\Users\Saad.LAKES\Desktop\Autoencoders\ReconVideo_Frames'
+EMBEDDING_SIZE = 1000
+SHAPE_REQUIRED = 720
+
+## MAIN
+names = read_video(PATH,'bunny_video.mp4')
+images = np.array(read_imgs(PATH,names,SHAPE_REQUIRED))
+images = images.astype('float')/255.
+print(images.shape)
+train,test = splitter(images,0.1)
+img_shape = np.prod(images.shape[1:])
+
+print('[INFO] The preprocessing is complete. Moving to model creation')
+
+model,encoder,decoder = build_model(images,EMBEDDING_SIZE,'CNN2',SHAPE_REQUIRED)
+model.summary()
+
+model_fit(model,train,test,200)
+
+model.load_weights('weights.hdf5')
+
+num_test = random.sample(range(0,len(test)),1)
+for i in num_test:
+    img = test[i]
+    visualize(img,encoder,decoder)
+
+bottle = encoder.predict(test)
+predicted = decoder.predict(bottle)
+
+print(f'The average Similarity image is : {round(overall_ssim(test,predicted),2)}')
+print(f'The compression rate is: {round((np.prod(img_shape)/EMBEDDING_SIZE),2)}')
+
+path = PATH_RESULTS.replace('\\','/')
+path_old = os.getcwd()
+os.chdir(path)
+# print(images.shape)
+# print(len(images))
+# print(images[0].shape)
+# print('Encoder:',encoder.predict(images[0]).shape)
+# temp = encoder.predict(images[0])
+# print('Decoder:',decoder.predict(temp).shape)
+
+out = cv2.VideoWriter('Reconstructed.avi',cv2.VideoWriter_fourcc(*"MJPG"),25.0,(720,720))
+for i in range(len(images)):
+    temp = np.expand_dims(images[i],axis=0)
+    preds = decoder.predict(encoder.predict(temp))
+    preds = preds.reshape(preds.shape[1],preds.shape[2],preds.shape[3])
+    preds = preds * 255.
+    # print(preds.shape)
+    cv2.imwrite('frame_{}.jpg'.format(i),preds)
+    out.write(preds)
+out.release
+os.chdir(path_old)
