91eebd68d538035e26e365f9803a6f0ef0bc43fa
Muhammad Saad <50722220+msaad1311@users.noreply.github.com>
2020-11-02 10:52:19 -0500

    updated the code to run on 256x256 images and the results are being saved

--- Videos/Code/main.py
+++ Videos/Code/main.py
@@ -21,13 +21,13 @@ if gpus:
         print(e)
         
 ## PARAMETERS
-PATH =  r'C:\Users\Saad.LAKES\Desktop\Autoencoders\Videos\Results\Video_Frames'
-PATH_RESULTS = r'C:\Users\Saad.LAKES\Desktop\Autoencoders\Videos\Results\ReconVideo_Frames'
+PATH =  r'C:\Users\saad\Desktop\Autoencoders\Videos\Results\Video_Frames'
+PATH_RESULTS = r'C:\Users\saad\Desktop\Autoencoders\Videos\Results\ReconVideo_Frames'
 EMBEDDING_SIZE = 1000
-SHAPE_REQUIRED = 128
+SHAPE_REQUIRED = 256
 
 ## MAIN
-names = read_video(PATH,r'C:\Users\Saad.LAKES\Desktop\Autoencoders\Videos\Dataset\bunny_video.mp4')
+names = read_video(PATH,r'C:\Users\saad\Desktop\Autoencoders\Videos\Dataset\bunny_video.mp4')
 images = np.array(read_imgs(PATH,names,SHAPE_REQUIRED))
 images = images.astype('float')/255.
 print(images.shape)
@@ -39,14 +39,14 @@ print('[INFO] The preprocessing is complete. Moving to model creation')
 model,encoder,decoder = build_model(images,EMBEDDING_SIZE,'CNN2',SHAPE_REQUIRED)
 model.summary()
 
-model_fit(model,train,test,200)
+# model_fit(model,train,test,200,5)
 
 model.load_weights('weights.hdf5')
 
-num_test = random.sample(range(0,len(test)),1)
-for i in num_test:
-    img = test[i]
-    visualize(img,encoder,decoder)
+# num_test = random.sample(range(0,len(test)),1)
+# for i in num_test:
+#     img = test[i]
+#     visualize(img,encoder,decoder)
 
 bottle = encoder.predict(test)
 predicted = decoder.predict(bottle)
@@ -58,14 +58,14 @@ path = PATH_RESULTS.replace('\\','/')
 path_old = os.getcwd()
 os.chdir(path)
 
-# out = cv2.VideoWriter('Reconstructed.avi',cv2.VideoWriter_fourcc(*"MJPG"),25.0,(128,128))
-# for i in range(len(images)):
-#     temp = np.expand_dims(images[i],axis=0)
-#     preds = decoder.predict(encoder.predict(temp))
-#     preds = preds.reshape(preds.shape[1],preds.shape[2],preds.shape[3])
-#     preds = preds * 255.
-#     # print(preds.shape)
-#     cv2.imwrite('frame_{}.jpg'.format(i),preds)
-#     out.write(preds)
-# out.release
+out = cv2.VideoWriter('Reconstructed.avi',cv2.VideoWriter_fourcc(*"MJPG"),25.0,(256,256))
+for i in range(len(images)):
+    temp = np.expand_dims(images[i],axis=0)
+    preds = decoder.predict(encoder.predict(temp))
+    preds = preds.reshape(preds.shape[1],preds.shape[2],preds.shape[3])
+    preds = preds * 255.
+    # print(preds.shape)
+    cv2.imwrite('frame_{}.jpg'.format(i),preds)
+    out.write(preds)
+out.release
 os.chdir(path_old)
